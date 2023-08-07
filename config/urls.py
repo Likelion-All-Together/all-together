e7b5894d0a8d1ce56bf40b2bbd154970d6632851
MinseoKangQ <kms02171@naver.com>
2023-08-08 00:35:41 +0900

    회원 정보 수정 구현

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
     path('accounts/', include('accounts.urls', namespace = 'accounts')),
     path('diamoyeo/', include('diamoyeo.urls', namespace = 'diamoyeo')),
 ]
+
+# 사용자가 추가한 프로필 이미지 저장을 위한 urlpatterns
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
