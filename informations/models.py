928941251fdb11970139330be40696316be29c65
MinseoKangQ <kms02171@naver.com>
2023-08-11 01:37:01 +0900

    informations 앱 DB 설계 완료

--- informations/models.py
+++ informations/models.py
@@ -1,7 +1,58 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
+User = get_user_model()
+
+SUPPORT_CYCLE_CHOICES = (
+    ('일', '일'),
+    ('월', '월'),
+    ('년', '년'),
+)
+
+TAG_CHOICES = (
+    ('태그1', '태그1'),
+    ('태그2', '태그2'),
+)
+
+INFORMATION_CATEGORY = (
+    ('지역', '지역'),
+    ('다문화', '다문화'),
+)
+
+DAY = (
+    ('월요일', '월요일'),
+    ('화요일', '화요일'),
+    ('수요일', '수요일'),
+    ('목요일', '목요일'),
+    ('금요일', '금요일'),
+    ('토요일', '토요일'),
+    ('일요일', '일요일'),
+)
+
+STATUS = (
+    ('접수 전', '접수 전'),
+    ('접수중', '접수중'),
+    ('접수마감', '접수마감'),
+)
 class RegionAndMulticultural(models.Model):
-    pass
+    title = models.CharField(verbose_name = '제목', max_length = 20, null = False, blank = False)
+    content = models.CharField(verbose_name = '내용', max_length = 100, null = False, blank = False)
+    phone = models.CharField(verbose_name = '전화번호', max_length = 20, null = False, blank = False)
+    support_cycle = models.CharField(verbose_name = '지원 주기', max_length = 20, choices = SUPPORT_CYCLE_CHOICES ,default = '일')
+    type = models.CharField(verbose_name = '제공 유형', max_length = 20, null = False, blank = False)
+    department = models.CharField(verbose_name = '담당 부서', max_length = 20, null = False, blank = False)
+    tag = models.CharField(verbose_name = '태그', max_length = 20, choices = TAG_CHOICES ,default = None) 
+    category = models.CharField(verbose_name = '지역/다문화 정보', max_length = 20, choices = INFORMATION_CATEGORY ,default = '일')
 
 class Afterschool(models.Model):
-    pass
+    region = models.CharField(verbose_name = '동 명', max_length = 20, null = False, blank = False)
+    title = models.CharField(verbose_name = '프로그램명', max_length = 20, null = False, blank = False)
+    receipt_start = models.DateField(verbose_name = '접수 시작 날짜')
+    receipt_end = models.DateField(verbose_name = '접수 마감 날짜')
+    training_start = models.DateField(verbose_name = '교육 시작 날짜')
+    training_end = models.DateField(verbose_name = '교육 마감 날짜')
+    day = models.CharField(verbose_name = '요일', max_length = 20, choices = DAY)
+    tuition = models.IntegerField(verbose_name = '수강료')
+    number_of_member = models.IntegerField(verbose_name = '정원')
+    status = models.CharField(verbose_name = '상태', max_length = 20, choices = STATUS, default = '접수중')
--- informations/views.py
+++ informations/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import RegionAndMulticultural, Afterschool
+
+# 회원가입
+def information_view(request):
+    
+    # GET 요청 시 HTML 응답
+    if request.method == 'GET':
+        region = RegionAndMulticultural.objects.filter(category='지역')
+        multicultural = RegionAndMulticultural.objects.filter(category='다문화')
+        afterschool = Afterschool.objects.all()
+        
+        context = {
+            'region_list' : region,
+            'multicultural_list' : multicultural,
+            'afterschool_list' : afterschool,
+        }
+        
+        return render(request, 'informations/home.html', context)
