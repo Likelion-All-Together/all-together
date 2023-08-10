1a296a5b3568dce95b2b5630a9b0dcae646d9bcb
MinseoKangQ <kms02171@naver.com>
2023-08-10 23:42:21 +0900

    informations 앱 추가 및 모델 작성중

---
+++ informations/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import RegionAndMulticultural, Afterschool
+
+admin.site.register(RegionAndMulticultural)
+admin.site.register(Afterschool)
---
+++ informations/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class RegionAndMulticultural(models.Model):
+    pass
+
+class Afterschool(models.Model):
+    pass
