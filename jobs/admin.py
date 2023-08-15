6c48e596e0e90face5591533a954c6a506659003
dhkang <alceste951005@naver.com>
2023-08-15 18:32:41 +0900

    과외하기 완료

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Class
+from .models import Class, Register
 # Register your models here.
 
 @admin.register(Class)
 class UserModelAdmin(admin.ModelAdmin):
     list_display = ('name','age','school','cost','id')
+
+@admin.register(Register)
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ('class_name','student_name','writer')
