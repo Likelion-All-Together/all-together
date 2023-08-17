a7ac6091d3d93455aebd5f5a231f06b133029126
dhkang <alceste951005@naver.com>
2023-08-18 04:07:30 +0900

    메인 완료

--- diamoyeo/views.py
+++ diamoyeo/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from posts.models import Post
+from jobs.models import Class
 
+import random
 def home_view(request):
-    return render(request, 'diamoyeo/home.html')
+        teacher_list = Class.objects.all()
+        random_teacher_list = random.sample(list(teacher_list), 4)
+        filter_list = Post.objects.exclude(writer__username = '관리자').order_by('-created_at') # 일반 사용자들 글
+        prandom_post_list = random.sample(list(filter_list), 3)
+        context = {
+            'teacher_list':random_teacher_list,
+            'post_list':prandom_post_list,
+        }
+        return render(request, 'diamoyeo/home.html',context)
