1a9255ae3ce25cebe20ef52683f43248530344fe
dhkang <alceste951005@naver.com>
2023-08-18 21:38:40 +0900

    마이페이지 결제목록, 스크랩

--- informations/urls.py
+++ informations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import information_view, scrap
+from .views import information_view, scrap, scrap_view_after,  scrap_view_region
 
 app_name = 'informations'
 
 urlpatterns = [
-    path('scrap/', scrap, name = 'scrap'), # 스크랩 기능
+    path('scrap/after/<int:bid>', scrap_view_after , name = 'scrap'), # 스크랩 기능
+    path('scrap/region/<int:bid>', scrap_view_region , name = 'scrap'), # 스크랩 기능
     path('', information_view, name = 'information'), # 탭에 따라 정보 보기
 ]
